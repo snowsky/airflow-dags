4220d326d5fa532aab45b1e8cb113dfac8bc1f08
Hao <hao.1.wang@gmail.com>
2023-12-22 11:41:02 -0500

    Update example_bash_operator_1.py

--- example_bash_operator_1.py
+++ example_bash_operator_1.py
@@ -27,7 +27,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="example_bash_operator",
+    dag_id="example_bash_operator_1",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
