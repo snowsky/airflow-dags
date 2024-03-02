5f6e59499c89b4cfc1e1535ea38b1c5c24d22e23
Hao <hao.1.wang@gmail.com>
2024-03-01 21:32:08 -0500

    Update example_bash_operator.py

--- example_bash_operator.py
+++ example_bash_operator.py
@@ -58,20 +58,20 @@ with DAG(
     # [START howto_operator_bash_template]
     also_run_this_1 = BashOperator(
         task_id="also_run_this_1",
-        bash_command="echo 'Hello from the container, konzaandssigrouppipelines, also_run_this!' >> /source-s3-konzaandssigrouppipelines/test.txt; sleep 60",
+        bash_command="echo 'Hello from the container, run1, also_run_this!' >> /source-s3-konzaandssigrouppipelines/test.txt; sleep 60",
     )
     # [END howto_operator_bash_template]
     also_run_this_1 >> run_this_last
 
     also_run_this_2 = BashOperator(
         task_id="also_run_this_2",
-        bash_command="echo 'Hello from the container, konzaandssigroupqa, also_run_this!' >> /source-s3-konzaandssigroupqa/test.txt; sleep 60",
+        bash_command="echo 'Hello from the container, run2, also_run_this!' >> /source-s3-konzaandssigroupqa/test.txt; sleep 60",
     )
     also_run_this_2 >> run_this_last
 
     also_run_this_3 = BashOperator(
         task_id="also_run_this_3",
-        bash_command="echo 'Hello from the container, konzaandssigroup, also_run_this!' >> /source-s3-konzaandssigroup/test.txt; sleep 60",
+        bash_command="echo 'Hello from the container, run3, also_run_this!' >> /source-s3-konzaandssigroup/test.txt; sleep 60",
     )
     also_run_this_3 >> run_this_last
 
