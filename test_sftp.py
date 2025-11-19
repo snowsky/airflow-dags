382bd809e14248fac10b5fd667072d920d419c73
Hao <hao.1.wang@gmail.com>
2025-11-18 20:00:54 -0500

    Refactor SFTP connection test function

--- test_sftp.py
+++ test_sftp.py
@@ -1,31 +1,21 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
 from airflow.providers.sftp.hooks.sftp import SFTPHook
-from airflow.utils.log.logging_mixin import LoggingMixin
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-def test_sftp_connection(**kwargs):
-    logger = LoggingMixin().log
-    params = kwargs.get('params', {})
-    sftp_conn_id = params.get('SFTP_CONN_ID', 'default_conn_id')
-    hook = SFTPHook(ssh_conn_id=sftp_conn_id)
-    try:
-        hook.get_conn()
-        logger.info(f"✅ SFTP connection successful using conn_id: {sftp_conn_id}")
-    except Exception as e:
-        logger.error(f"❌ SFTP connection failed for conn_id {sftp_conn_id}: {e}", exc_info=True)
-        raise
+def test_sftp(**kwargs):
+    hook = SFTPHook(ssh_conn_id="my_sftp")
+    print("Listing remote directory:")
+    print(hook.list_directory("/"))
 
 with DAG(
-    dag_id='test_sftp_connection_from_conn_id',
-    start_date=datetime(2025, 10, 8),
+    "sftp_ed25519_test",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
     catchup=False,
-    params={
-        'SFTP_CONN_ID': 'sftp' #default value
-    }
 ) as dag:
 
-    test_connection = PythonOperator(
-        task_id='test_sftp_connection',
-        python_callable=test_sftp_connection,
+    PythonOperator(
+        task_id="test_sftp_conn",
+        python_callable=test_sftp,
     )
