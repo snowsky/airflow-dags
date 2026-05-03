86dd42b0efc6d388c8f59fe5d7e2b2c14c850784
Hao <hao.1.wang@gmail.com>
2026-05-03 15:07:09 -0400

    Create airflow_variable_manager.py to manage Airflow variables

---
+++ airflow_variable_manager.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
+from datetime import datetime
+
+
+def get_and_update_variable():
+    # Get the existing value of the variable
+    var_name = 'my_variable'
+    existing_value = Variable.get(var_name, default_var='default_value')
+    
+    # Update the variable value
+    new_value = existing_value + '_updated'
+    Variable.set(var_name, new_value)
+
+
+# Define the DAG
+with DAG(
+    dag_id='airflow_variable_manager',
+    schedule_interval='@daily',  # Run once a day
+    start_date=datetime(2026, 5, 1),
+    catchup=False,
+) as dag:
+
+    update_variable_task = PythonOperator(
+        task_id='update_variable',
+        python_callable=get_and_update_variable,
+    )
